9d2dc52ac05a64344353a8c8c6b505b8c90cdeef
Dominik Rohal <rohal.dominik.123@gmail.com>
2024-01-31 09:15:57 +0200

    commented out timestream

--- main.tf
+++ main.tf
@@ -13,40 +13,40 @@ resource "aws_kinesis_stream" "aq_data_stream" {
   }
 }
 
-resource "aws_timestreamwrite_database" "aq_time_stream" {
-  database_name = "aq-time-stream"
+#TODO: uncomment to enable timestream
+#resource "aws_timestreamwrite_database" "aq_time_stream" {
+#  database_name = "aq-time-stream"
+#
+#  tags = {
+#    Flow = "timestream"
+#  }
+#}
 
-  tags = {
-    Flow = "timestream"
-  }
-}
-
-
-resource "aws_timestreamwrite_table" "aq_time_stream_table" {
-  #  https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/timestreamwrite_table
-  database_name = aws_timestreamwrite_database.aq_time_stream.database_name
-  table_name    = "aq_data"
-
-  schema {
-    composite_partition_key {
-      enforcement_in_record = "REQUIRED"
-      name                  = "device"
-      type                  = "DIMENSION"
-    }
-  }
-
-  magnetic_store_write_properties {
-    enable_magnetic_store_writes = true
-  }
-
-  retention_properties {
-#    TODO: adjust retention period values!!!
-    magnetic_store_retention_period_in_days = 1
-    memory_store_retention_period_in_hours  = 1
-  }
-
-#  TODO: partition key
-}
+#TODO: uncomment to enable timestream
+#resource "aws_timestreamwrite_table" "aq_time_stream_table" {
+#  #  https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/timestreamwrite_table
+#  database_name = aws_timestreamwrite_database.aq_time_stream.database_name
+#  table_name    = "aq_data"
+#
+#  schema {
+#    composite_partition_key {
+#      enforcement_in_record = "REQUIRED"
+#      name                  = "device"
+#      type                  = "DIMENSION"
+#    }
+#  }
+#
+#  magnetic_store_write_properties {
+#    enable_magnetic_store_writes = true
+#  }
+#
+#  retention_properties {
+##    TODO: adjust retention period values!!!
+#    magnetic_store_retention_period_in_days = 1
+#    memory_store_retention_period_in_hours  = 1
+#  }
+#
+#}
 
 resource "aws_iot_topic_rule" "rule" {
   #  IoT topic rule to direct data published in MQTT topic to the Kinesis Data Stream
@@ -62,19 +62,20 @@ resource "aws_iot_topic_rule" "rule" {
     partition_key = "$${device}"
   }
 
-  timestream {
-    database_name = aws_timestreamwrite_database.aq_time_stream.database_name
-    table_name = aws_timestreamwrite_table.aq_time_stream_table.table_name
-    role_arn = aws_iam_role.iot_role.arn
-    dimension {
-      name  = "device"
-      value = "$${device}"
-    }
-    timestamp {
-      unit  = "MILLISECONDS"
-      value = "$${time_to_epoch(time, 'yyyy-MM-dd HH:mm:ss')}"
-    }
-  }
+#  TODO: uncomment to enable timestream
+#  timestream {
+#    database_name = aws_timestreamwrite_database.aq_time_stream.database_name
+#    table_name = aws_timestreamwrite_table.aq_time_stream_table.table_name
+#    role_arn = aws_iam_role.iot_role.arn
+#    dimension {
+#      name  = "device"
+#      value = "$${device}"
+#    }
+#    timestamp {
+#      unit  = "MILLISECONDS"
+#      value = "$${time_to_epoch(time, 'yyyy-MM-dd HH:mm:ss')}"
+#    }
+#  }
 }
 
 
@@ -113,42 +114,44 @@ resource "aws_iam_policy" "kinesis_publish_policy" {
   })
 }
 
-resource "aws_iam_policy" "timestream_publish_policy" {
-  name = "timestream_publish_policy"
-  description = "Policy to allow publishing to Timestream"
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action   = [
-          "timestream:WriteRecords",
-          "timestream:WriteRecords",
-        ],
-        Effect   = "Allow",
-        Resource = aws_timestreamwrite_table.aq_time_stream_table.arn
-      },
-      {
-        Action = [
-          "timestream:DescribeEndpoints",
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      }
-      # Add more statements as needed for other permissions
-    ]
-  })
-}
+#TODO: uncomment to enable timestream
+#resource "aws_iam_policy" "timestream_publish_policy" {
+#  name = "timestream_publish_policy"
+#  description = "Policy to allow publishing to Timestream"
+#
+#  policy = jsonencode({
+#    Version = "2012-10-17",
+#    Statement = [
+#      {
+#        Action   = [
+#          "timestream:WriteRecords",
+#          "timestream:WriteRecords",
+#        ],
+#        Effect   = "Allow",
+#        Resource = aws_timestreamwrite_table.aq_time_stream_table.arn
+#      },
+#      {
+#        Action = [
+#          "timestream:DescribeEndpoints",
+#        ]
+#        Effect   = "Allow"
+#        Resource = "*"
+#      }
+#      # Add more statements as needed for other permissions
+#    ]
+#  })
+#}
 
 resource "aws_iam_role_policy_attachment" "kinesis_publish_attachment" {
   policy_arn = aws_iam_policy.kinesis_publish_policy.arn
   role       = aws_iam_role.iot_role.name
 }
 
-resource "aws_iam_role_policy_attachment" "timestream_publish_attachment" {
-  policy_arn = aws_iam_policy.timestream_publish_policy.arn
-  role       = aws_iam_role.iot_role.name
-}
+#TODO: uncomment to enable timestream
+#resource "aws_iam_role_policy_attachment" "timestream_publish_attachment" {
+#  policy_arn = aws_iam_policy.timestream_publish_policy.arn
+#  role       = aws_iam_role.iot_role.name
+#}
 
 ## Optionally, you might want to output the IAM role ARN for reference
 output "iot_kinesis_role_arn" {
