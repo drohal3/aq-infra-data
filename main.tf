9b0dad38c7c96a307fdf3ba7cac26239e4f5109e
Dominik Rohal <rohal.dominik.123@gmail.com>
2024-01-31 16:43:59 +0200

    in progress... kinesis

--- main.tf
+++ main.tf
@@ -48,12 +48,11 @@ resource "aws_kinesis_stream" "aq_data_stream" {
 #
 #}
 
-resource "aws_iot_topic_rule" "rule" {
-  #  IoT topic rule to direct data published in MQTT topic to the Kinesis Data Stream
-  name        = "AQ_MeasurementRule"
-  description = "IoT Topic Rule for AQ measurements"
+resource "aws_iot_topic_rule" "aq_kinesis_rule" {
+  name        = "AQ_Kinesis_Measurement_Rule"
+  description = "IoT Topic Kinesis Rule for AQ measurements"
   enabled     = true
-  sql         = "SELECT temperature, humidity, time FROM 'aq/measurement'"
+  sql         = "SELECT device, temperature, humidity, time FROM 'aq/measurement'"
   sql_version = "2016-03-23"
 
   kinesis {
@@ -61,8 +60,17 @@ resource "aws_iot_topic_rule" "rule" {
     stream_name = aws_kinesis_stream.aq_data_stream.name
     partition_key = "$${device}"
   }
+}
 
 #  TODO: uncomment to enable timestream
+#resource "aws_iot_topic_rule" "aq_timestream_rule" {
+#  #  IoT topic rule to direct data published in MQTT topic to the Kinesis Data Stream
+#  name        = "AQ_Timestream_MeasurementRule"
+#  description = "IoT Topic Timestream Rule for AQ measurements"
+#  enabled     = true
+#  sql         = "SELECT temperature, humidity, time FROM 'aq/measurement'"
+#  sql_version = "2016-03-23"
+
 #  timestream {
 #    database_name = aws_timestreamwrite_database.aq_time_stream.database_name
 #    table_name = aws_timestreamwrite_table.aq_time_stream_table.table_name
@@ -76,7 +84,7 @@ resource "aws_iot_topic_rule" "rule" {
 #      value = "$${time_to_epoch(time, 'yyyy-MM-dd HH:mm:ss')}"
 #    }
 #  }
-}
+#}
 
 
 # IAM
@@ -162,11 +170,185 @@ output "kinesis_stream_arn" {
   value = aws_kinesis_stream.aq_data_stream.arn
 }
 
+
+########################################################################################################################
+########################################################################################################################
+# Kinesis Firehouse -> S3
+resource "aws_kinesis_firehose_delivery_stream" "extended_s3_stream" {
+  name        = "terraform-kinesis-firehose-extended-s3-test-stream"
+  destination = "extended_s3"
+
+  extended_s3_configuration {
+    role_arn   = aws_iam_role.firehose_role.arn
+    bucket_arn = aws_s3_bucket.measurements_bucket.arn
+
+    buffering_size = 65
+    buffering_interval = 60
+
+    # https://docs.aws.amazon.com/firehose/latest/dev/dynamic-partitioning.html
+    dynamic_partitioning_configuration {
+      enabled = "true"
+    }
+
+    # Example prefix using partitionKeyFromQuery, applicable to JQ processor
+    prefix              = "data/device=!{partitionKeyFromQuery:device}/year=!{timestamp:yyyy}/month=!{timestamp:MM}/day=!{timestamp:dd}/hour=!{timestamp:HH}/"
+    error_output_prefix = "errors/year=!{timestamp:yyyy}/month=!{timestamp:MM}/day=!{timestamp:dd}/hour=!{timestamp:HH}/!{firehose:error-output-type}/"
+
+    processing_configuration {
+      enabled = "true"
+
+      # Multi-record deaggregation processor example
+      processors {
+        type = "RecordDeAggregation"
+        parameters {
+          parameter_name  = "SubRecordType"
+          parameter_value = "JSON"
+        }
+      }
+
+      # New line delimiter processor example
+      processors {
+        type = "AppendDelimiterToRecord"
+      }
+
+      # JQ processor example
+      processors {
+        type = "MetadataExtraction"
+        parameters {
+          parameter_name  = "JsonParsingEngine"
+          parameter_value = "JQ-1.6"
+        }
+        parameters {
+          parameter_name  = "MetadataExtractionQuery"
+          parameter_value = "{device:.device}"
+        }
+      }
+    }
+  }
+
+  kinesis_source_configuration {
+    kinesis_stream_arn  = aws_kinesis_stream.aq_data_stream.arn
+    role_arn            = aws_iam_role.firehose_role.arn
+  }
+}
+
 resource "aws_s3_bucket" "measurements_bucket" {
   bucket = "idealaq-aq-measurements-bucket"
 
   tags = {
     Name        = "AQ measurements bucket"
-    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.measurements_bucket.id
+  acl    = "private"
+  depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
+}
+
+resource "aws_iam_role" "firehose_role" {
+  name = "firehouse_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "firehose.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "firehose_publish_policy" {
+  name        = "firehose_publish_policy"
+  description = "Policy to allow publishing to Kinesis Delivery Stream"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action   = ["firehose:PutRecord", "firehose:PutRecordBatch"],
+        Effect   = "Allow",
+        Resource = aws_kinesis_firehose_delivery_stream.extended_s3_stream.arn
+      }
+      # Add more statements as needed for other permissions
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "put_firehose_record" {
+  role       = aws_iam_role.firehose_role.name
+  policy_arn = aws_iam_policy.firehose_publish_policy.arn
+}
+
+resource "aws_iam_policy" "s3_publish_policy" {
+  name        = "s3_publish_policy"
+  description = "Policy to allow publishing to s3"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action   = [
+          "s3:AbortMultipartUpload",
+          "s3:GetBucketLocation",
+          "s3:GetObject",
+          "s3:ListBucket",
+          "s3:ListBucketMultipartUploads",
+          "s3:PutObject"
+        ],
+        Effect   = "Allow",
+        Resource = [
+          aws_s3_bucket.measurements_bucket.arn,
+          "${aws_s3_bucket.measurements_bucket.arn}/*"
+        ]
+      }
+      # Add more statements as needed for other permissions
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "put_s3_record" {
+  role       = aws_iam_role.firehose_role.name
+  policy_arn = aws_iam_policy.s3_publish_policy.arn
+}
+
+
+resource "aws_iam_policy" "data_stream_policy" {
+  name        = "data_stream_policy"
+  description = "Data Stream Policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action   = [
+          "kinesis:DescribeStream"
+        ],
+        Effect   = "Allow",
+        Resource = [
+          aws_kinesis_stream.aq_data_stream.arn
+        ]
+      }
+      # Add more statements as needed for other permissions
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "data_stream_policy_attachment" {
+  role       = aws_iam_role.firehose_role.name
+  policy_arn = aws_iam_policy.data_stream_policy.arn
+}
+
+# https://stackoverflow.com/questions/76049290/error-accesscontrollistnotsupported-when-trying-to-create-a-bucket-acl-in-aws
+# Resource to avoid error "AccessControlListNotSupported: The bucket does not allow ACLs"
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
+  bucket = aws_s3_bucket.measurements_bucket.id
+  rule {
+    object_ownership = "ObjectWriter"
   }
 }
